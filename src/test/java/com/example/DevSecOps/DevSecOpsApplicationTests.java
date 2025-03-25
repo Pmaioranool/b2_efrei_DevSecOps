4a0a985f921cd1cbc85000485b1aa4b244293bb1
Pmaioranool <lucasmaiorano77@gmail.com>
2025-03-25 15:38:07 +0100

    newcarservice

--- src/test/java/com/example/DevSecOps/DevSecOpsApplicationTests.java
+++ src/test/java/com/example/DevSecOps/DevSecOpsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.DevSecOps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +9,7 @@ class DevSecOpsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		car car = new car("AA11BB", "ferrari", 2000);
+		Car car = new Car("AA11BB", "ferrari", 2000);
 		String plate = car.getPlateNumber();
 
 		assertEquals("AA11BB", plate);
